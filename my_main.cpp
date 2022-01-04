62c9c3bdaf7a06a39fb63f7d919f0a39cbb57e81
Kallistina <despotidou02@gmail.com>
2022-01-05 00:22:46 +0200

    the_start_of_main

---
+++ my_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "INCLUDES/attikh_odos.h"
+#include "INCLUDES/entrance.h"
+#include "INCLUDES/section.h"
+#include "INCLUDES/toll.h"
+#include "INCLUDES/vehicle.h"
+
+
+int main() {
+
+}
